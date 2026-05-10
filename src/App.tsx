090d4b7c8430798a7ca7da98dd38326f4225b25e
Sumayea Rahman <shorbonam104@gmail.com>
2026-05-11 03:50:23 +0600

    initial setup vite typescript tailwind shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+    return (
+        <div className="App">
+          <h1>react + tailwindcss + shadcn/ui</h1>
+          <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+        </div>
+    )
+}
+
+export default App
